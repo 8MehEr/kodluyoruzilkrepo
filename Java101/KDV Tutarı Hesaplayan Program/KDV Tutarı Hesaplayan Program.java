3996f631c52326ea8feff0efacdd22052c225b68
8MehEr <meh18met@hotmail.com>
2022-09-16 19:22:06 +0300

    KDV Projesi

---
+++ Java101/KDV Tutarı Hesaplayan Program/KDV Tutarı Hesaplayan Program.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double Fiyat,KDV = 0.18,KDVFiyat,KDVS;
+
+        System.out.print("Fiyatı Giriniz :");
+        Fiyat = input.nextDouble();
+        KDVS = Fiyat * KDV;
+        KDVFiyat = KDVS + Fiyat;
+
+        System.out.println("KDV'siz Fiyat :" + Fiyat );
+        System.out.println("KDV'li Fiyat :" + KDVFiyat);
+        System.out.println("KDV Oranı :" + KDV);
+        System.out.print("KDV :" + KDVS);
+
+    }
+}
